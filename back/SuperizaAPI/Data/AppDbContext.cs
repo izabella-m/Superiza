4a1d3911a1c6e0597a0b7d47b325114552109575
izabella-m <izabella.pessoa03@outlook.com>
2025-02-27 11:45:52 -0300

    Configurações de banco de dados

---
+++ back/SuperizaAPI/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using SuperizaAPI.Models;
+
+
+namespace SuperizaAPI.Data;
+
+public class AppDbContext : DbContext
+{
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<FornecedorModel> Fornecedores { get; set; }
+    public DbSet<ProdutoModel> Produtos { get; set; }
+    public DbSet<PedidoModel> Pedidos { get; set; }
+    public DbSet<ItemPedidoModel> ItensPedido { get; set; }
+}
+
+
