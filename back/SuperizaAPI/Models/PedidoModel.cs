1f1b2be1a67d672de1d92f6f16887b572d0a823c
izabella-m <izabella.pessoa03@outlook.com>
2025-02-27 12:27:11 -0300

    mudança na relação de ItemPedido para Produtos

--- back/SuperizaAPI/Models/PedidoModel.cs
+++ back/SuperizaAPI/Models/PedidoModel.cs
@@ -2,12 +2,12 @@ namespace SuperizaAPI.Models;
 
 public class PedidoModel
 {
-    public int Id { get; set; }              // Identificador único do pedido
-    public DateTime DataPedido { get; set; } // Data do pedido
-    public DateTime DataEntrega { get; set; } // Data de entrega
+    public int Id { get; set; }             
+    public DateTime DataPedido { get; set; } 
+    public DateTime DataEntrega { get; set; } 
 
     public int FornecedorId { get; set; }  // Chave estrangeira
     public FornecedorModel Fornecedor { get; set; }  // Relacionamento com Fornecedor (sem o "Id")
-
-    public ICollection<ItemPedidoModel> ItensPedido { get; set; }
+    
+    public string Produtos { get; set; }
 }
